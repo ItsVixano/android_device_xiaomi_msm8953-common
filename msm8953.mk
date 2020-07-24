6f3d7e162acbf66c4544e5daa63b21b24bf77b40
vitor00almei <pondebatata@outlook.com>
2020-07-24 18:02:00 -0300

    msm8953-common: remove ims products from tree
    *already defined on source
    
    Signed-off-by: vitor00almei <pondebatata@outlook.com>
    Change-Id: I494e8eb275282ef78ad3d6de2a08dd204a7a7319

--- msm8953.mk
+++ msm8953.mk
@@ -193,11 +193,6 @@ hardware/qcom/display
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
 
-# IMS
-PRODUCT_PACKAGES += \
-    ims-ext-common \
-    ims_ext_common.xml
-
 # Input
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/keylayout/ft5435_ts.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/ft5435_ts.kl \
