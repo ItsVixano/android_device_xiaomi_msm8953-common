705c82a12912d02db0ffc8c3d8d34965f09edc8f
vitor00almei <pondebatata@outlook.com>
2020-08-01 21:39:55 -0300

    msm8953-common: uprev display.config to 1.9
    Change-Id: Ia3b438eebbce66926114bde237d4145522d970e8

--- msm8953.mk
+++ msm8953.mk
@@ -148,7 +148,9 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libqdMetaData.system \
     libgenlock \
-    libtinyxml
+    libtinyxml \
+    vendor.display.config@1.9 \
+    vendor.display.config@1.9_vendor
 
 # DRM
 PRODUCT_PACKAGES += \
