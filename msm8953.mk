a70335bcc9e36d897dbcd95249b16e7324fece02
ItsVixano <vixanonew@gmail.com>
2020-11-13 22:51:29 +0100

    msm8953-common: switch to AOSP charging deamon
    * bgcngm miui implementation works fine for 1080p devices, not for 720p devices like ysl
    
    Signed-off-by: ItsVixano <vixanonew@gmail.com>
    Change-Id: I682c84f37bb749eda5c3b0472601edc0bac7a9e3

--- msm8953.mk
+++ msm8953.mk
@@ -191,10 +191,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/configs/gps/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
 
-# Healthd
-PRODUCT_PACKAGES += \
-    chargeonlymode
-
 # HW crypto
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.cryptfshw@1.0-service-qti.qsee
