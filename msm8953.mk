b7b48150f044c7e5c1d47b01f095f720e7d15ce4
Michael Bestas <mkbestas@lineageos.org>
2021-01-12 17:16:03 +0530

    msm8953-common: Remove android.hardware.graphics.composer@2.1-impl
    It no longer exists, the service is fully binderized
    
    Change-Id: I06ee402b50216948e0e72a2797c2edfda1c4886a

--- msm8953.mk
+++ msm8953.mk
@@ -145,7 +145,6 @@ PRODUCT_PACKAGES += \
     gralloc.msm8953
 
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.1-impl \
     android.hardware.graphics.composer@2.1-service \
     hwcomposer.msm8953
 
