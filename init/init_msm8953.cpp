f470576c87fb69f791acd7de948d34ae2a90b7c3
ItsVixano <vixanonew@gmail.com>
2021-06-27 15:14:43 +0200

    msm8953-common: init: Properly checkout dalvik props
    * from:
    - https://github.com/LineageOS/android_frameworks_native/blob/lineage-18.1/build/phone-xhdpi-4096-dalvik-heap.mk
    - https://github.com/LineageOS/android_frameworks_native/blob/lineage-18.1/build/phone-xhdpi-2048-dalvik-heap.mk
    
    * for getting ~3% of improvments "before https://i.imgur.com/8uUohYT.jpeg; after https://i.imgur.com/DyM6UTh.jpeg"
    * also apply directly common props without checking them
    
    Change-Id: I36b422cc068da1a8e98c4ee112641fb1d18f5585

--- init/init_msm8953.cpp
+++ init/init_msm8953.cpp
@@ -69,17 +69,11 @@ void check_device()
 
     if (sys.totalram > 3072ull * 1024 * 1024) {
         // from - phone-xxhdpi-4096-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "256m";
-        heapsize = "512m";
         heaptargetutilization = "0.6";
         heapminfree = "8m";
         heapmaxfree = "16m";
     } else {
         // from - phone-xhdpi-2048-dalvik-heap.mk
-        heapstartsize = "8m";
-        heapgrowthlimit = "192m";
-        heapsize = "512m";
         heaptargetutilization = "0.75";
         heapminfree = "512k";
         heapmaxfree = "8m";
@@ -90,9 +84,9 @@ void vendor_load_properties()
 {
     check_device();
 
-    property_override("dalvik.vm.heapstartsize", heapstartsize);
-    property_override("dalvik.vm.heapgrowthlimit", heapgrowthlimit);
-    property_override("dalvik.vm.heapsize", heapsize);
+    property_override("dalvik.vm.heapstartsize", "8m");
+    property_override("dalvik.vm.heapgrowthlimit", "192m");
+    property_override("dalvik.vm.heapsize", "512m");
     property_override("dalvik.vm.heaptargetutilization", heaptargetutilization);
     property_override("dalvik.vm.heapminfree", heapminfree);
     property_override("dalvik.vm.heapmaxfree", heapmaxfree);
