ec3867a5522c8cdcd209e1e86f9444cedb1f4f79
ItsVixano <vixanonew@gmail.com>
2021-07-16 12:35:49 +0200

    msm8953-common: My name is ysl
    Change-Id: I3dcf8dbbaf69715a76b546ad036fea872181b667

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter mido tissot vince daisy,$(TARGET_DEVICE)),)
+ifneq ($(filter mido tissot vince daisy ysl,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
