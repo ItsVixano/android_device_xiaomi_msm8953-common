a4de53f4023a0650cad0f0ee90fcf07d3072bfb9
ItsVixano <vixanonew@gmail.com>
2021-07-16 12:34:12 +0200

    msm8953-common: Decommonize Screen Aapt config
    * ysl uses a 720p display instead of 1080p
    
    Change-Id: I5e39eb8e486bb69fe526005a6bde9fa0d09d42b9

--- msm8953.mk
+++ msm8953.mk
@@ -13,7 +13,6 @@ PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += $(LOCAL_PATH)/overlay/packages/apps/Car
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
 # Permissions
 PRODUCT_COPY_FILES += \
