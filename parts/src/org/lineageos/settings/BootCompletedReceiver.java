0707375c682ce05fc6f0aee73dc1eea8077cc4b6
ItsVixano <vixanonew@gmail.com>
2021-10-31 17:19:09 +0100

    msm8953-common: parts: Cleanup AOD code
    * We'll never see a sdm625 device with an amoled display
    
    Change-Id: Ice7e2808d0e402a4298f1a4843960ddfbf1b7d34

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
- *               2017-2019 The LineageOS Project
+ *               2017-2018 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,8 +32,10 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DEBUG) Log.d(TAG, "Received boot completed intent");
+        if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
+            if (DEBUG) Log.d(TAG, "Starting Doze service");
+            DozeUtils.startService(context);
+        }
         DiracUtils.initialize();
-        DozeUtils.checkDozeService(context);
     }
 }
