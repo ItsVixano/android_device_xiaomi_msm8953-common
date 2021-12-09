135a752e7a516b3bb36d77c8d0d904087d3c4eb6
ItsVixano <vixanonew@gmail.com>
2021-12-09 11:47:52 +0100

    Revert "msm8953-common: Build custom lights AIDL"
    This reverts commit af524b5a32240e2fa7337498ec7c65b6e0c2d886.
    
    Change-Id: I8be96cee40025e36b57a7725452ff8c42e1dab8b

--- msm8953.mk
+++ msm8953.mk
@@ -210,6 +210,12 @@ PRODUCT_PACKAGES += \
     ipacm \
     IPACM_cfg.xml
 
+# Lights
+PRODUCT_PACKAGES += \
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service \
+    lights.msm8953
+
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.0-service-sdm
