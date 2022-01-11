e9b82cca1e2c1f2bf0decf9a158a86c7696d3217
ItsVixano <giovanniricca@protonmail.com>
2022-01-12 00:31:04 +0100

    msm8953-common: Cleanup CNE blobs
    Change-Id: I87c3cae126a94034b89d1fafc594ba352e5390e1

--- extract-files.sh
+++ extract-files.sh
@@ -64,9 +64,6 @@ fi
 
 function blob_fixup() {
     case "${1}" in
-        product/etc/permissions/vendor.qti.hardware.data.connection-V1.0-java.xml|product/etc/permissions/vendor.qti.hardware.data.connection-V1.1-java.xml)
-            sed -i 's/version="2.0"/version="1.0"/g' "${2}"
-            ;;
         product/lib64/libdpmframework.so)
             for LIBSHIM_DPMFRAMEWORK in $(grep -L "libshim_dpmframework.so" "${2}"); do
                 "${PATCHELF}" --add-needed "libshim_dpmframework.so" "${2}"
