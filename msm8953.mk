d4e47c11a7ae4a25401c530dfaf4111111fefce0
Giovanni Ricca <giovanniricca@protonmail.com>
2022-03-15 16:30:34 +0100

    msm8953-common: Provide gnss, power, displayservice on vendor
    Change-Id: I8bc8dcc9a4f962123eee3130f9de3ae467f40a98

--- msm8953.mk
+++ msm8953.mk
@@ -129,6 +129,7 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
+    android.frameworks.displayservice@1.0.vendor \
     android.hardware.graphics.allocator@2.0-impl \
     android.hardware.graphics.allocator@2.0-service \
     android.hardware.graphics.mapper@2.0-impl-2.1 \
@@ -169,6 +170,12 @@ PRODUCT_PACKAGES += \
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
+
+# GNSS
+PRODUCT_PACKAGES += \
+    android.hardware.gnss@1.2.vendor \
+    android.hardware.gnss@2.1.vendor \
+    android.hardware.gnss@3.0.vendor
 
 # Health
 PRODUCT_PACKAGES += \
@@ -263,6 +270,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
+    android.hardware.power@1.2.vendor \
     android.hardware.power-service-qti
 
 PRODUCT_COPY_FILES += \
